bb466e435a45ceef8589b5d1024f12334a5b6fbd
yasna2002 <yasnatalebi@gmail.com>
2023-01-01 12:14:04 +0330

    created a dictionary of files based on date over!

--- project.py
+++ project.py
@@ -1,16 +1,17 @@
 # DS Final Project
 import os
+from collections import defaultdict
 from zipfile import ZipFile
 
 
-def unzip(): # unzipping the main zip file
-    with ZipFile("E:\\Data Structure\\DS_Project\\Main.zip", 'r') as zObject:
-        os.mkdir(path="E:/Data Structure/DS_Project/Main")  # unzips here
+def unzip():  # unzipping the main zip file
+    with ZipFile("D:\\programs\\Github\\ds-project-olympians-ii\\Main.zip", 'r') as zObject:
+        os.mkdir(path="D:/programs/Github/ds-project-olympians-ii/Main")  # unzips here
 
-        zObject.extractall(path="E:\\Data Structure\\DS_Project\\Main")
+        zObject.extractall(path="D:\\programs\\Github\\ds-project-olympians-ii\\Main")
 
 
-def find_dirs(root_dir, year_dict): # finding all folders
+def find_dirs(root_dir, year_dict):  # finding all folders
     for file in os.listdir(root_dir):  # getting all the files and folders
         folder = os.path.join(root_dir, file)
         if os.path.isdir(folder):  # finding the folders
@@ -20,13 +21,22 @@ def find_dirs(root_dir, year_dict): # finding all folders
                 new_dir = os.path.join(root_dir, files)
                 os.rename(old_dir, new_dir)  # changing dir of files
 
+                if os.path.isfile(new_dir):  # finding files between files and folders
+                    date = os.path.basename(new_dir).split('/')[-1].split('.')[-2]
+
+                    if int(date) <= 2022:
+                        year_dict[date].append(files)  # creating a dictionary of files by date key
+                    else:
+                        os.remove(new_dir)  # remove a file with invalid date
+
+            # dict(sorted(year_dict.items()))
             os.rmdir(folder)  # deleting the folder
 
-            find_dirs(root_dir)
+            find_dirs(root_dir, year_dict)
 
 
 if __name__ == '__main__':
     unzip()
-    root_dir = "E:\\Data Structure\\DS_Project\\Main"
-    year_dict = {}
+    root_dir = "D:\\programs\\Github\\ds-project-olympians-ii\\Main"
+    year_dict = defaultdict(list)
     find_dirs(root_dir, year_dict)
