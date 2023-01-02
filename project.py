fb70d434ad747f1ffc96deb2b4ef206b1338cd56
Ilya Shabanpour Foladi <ilyashabanpour@gmail.com>
2023-01-02 14:53:57 +0330

    merged with Yasna(added phase2) (debugged)

--- project.py
+++ project.py
@@ -129,16 +129,6 @@ def redo():
     print("File recreated!")
 
 
-def date_order(root_dir, year_dict):  # sorting files by date
-    os.mkdir(path="D:/programs/Github/ds-project-olympians-ii/Main/folder")  # creating a temp folder
-    temp = "D:/programs/Github/ds-project-olympians-ii/Main/folder"
-    for key in sorted(year_dict.keys()):  # moving sorted files to the temp dir
-        for file in range(len(year_dict[key])):
-            old_dir = os.path.join(root_dir, year_dict[key][file])
-            new_dir = os.path.join(temp, year_dict[key][file])
-            os.rename(old_dir, new_dir)
-
-
 def folder_creator(year_dict, root_dir):
     for key in sorted(year_dict.keys()):  # moving sorted files to the temp dir
         os.mkdir(path="D:/programs/Github/ds-project-olympians-ii/Main/" + str(key))
